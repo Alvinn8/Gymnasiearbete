f50fc3fe7b4773726dd10e44230381cec7a9ca3e
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2023-01-16 11:09:14 +0100

    Test Vue.js

---
+++ frontend/src/views/TestView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+
+const counter = useCounterStore();
+</script>
+
+<template>
+    <div>
+        <p>Hello {{ counter.count }}</p>
+        <button @click="counter.increment">Increment the counter</button>
+    </div>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  div {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
